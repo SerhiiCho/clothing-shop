0e2706df9fd42ac529e928ef2c67f4b6bf0d87f5
Serhii Cho <1990serzhil@gmail.com>
2018-05-03 10:42:15 +0300

    moved bootstrap to its own file instead of compiling it every save

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
     <title>{{ env('APP_NAME', 'Clothing Shop') }}</title>
+	<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<link rel="shortcut icon" href="{{ asset('storage/img/favicon.png') }}" type="image/x-icon">
 </head>
