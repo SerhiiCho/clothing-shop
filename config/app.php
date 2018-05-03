9950166d4a4aba42aa167d5c11d4bc8f7c794cb7
Serhii Cho <1990serzhil@gmail.com>
2018-05-03 15:56:22 +0300

    renamed ComposerProvider and added FooterProvider

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ComposerProvider::class,
+        App\Providers\FooterProvider::class,
 
     ],
 
