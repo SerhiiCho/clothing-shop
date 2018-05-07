23fa45b54c0b3dbaf2663d1005cd0225165adb09
Serhii Cho <1990serzhil@gmail.com>
2018-05-07 17:11:41 +0300

    Changed migration

--- database/migrations/2018_05_07_143634_create_slider_table.php
+++ database/migrations/2018_05_07_143634_create_slider_table.php
@@ -16,7 +16,7 @@ class CreateSliderTable extends Migration
         Schema::create('slider', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('image', 100);
-            $table->integer('order');
+            $table->integer('order')->nullable();
         });
     }
 
