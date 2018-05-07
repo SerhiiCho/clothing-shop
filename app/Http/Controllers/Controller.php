e7c2a854f5d4ffd4bc82e37081e9e48d1c89b0e8
Serhii Cho <1990serzhil@gmail.com>
2018-05-07 14:07:47 +0300

    Added log function to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +11,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	
+	public function log($message, $file_name = 'errors.log')
+	{
+		$view_log = new Logger('errors');
+		$view_log->pushHandler(new StreamHandler(
+			storage_path('/logs/' . $file_name), Logger::INFO
+		));
+		$view_log->addInfo($message);
+	}
 }
