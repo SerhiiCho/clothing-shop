6c005d2b01516db551c895167c2d6026f92c6349
Serhii Cho <1990serzhil@gmail.com>
2018-05-11 20:54:48 +0300

    Cart translations

---
+++ resources/lang/ru/cart.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+	// View
+	'cart' => 'Корзина',
+	'product' => 'Продукт',
+	'price' => 'Цена',
+	'quantity' => 'Кол-во',
+	'total' => 'Всего',
+	'delete' => 'Удалить',
+	'order' => 'Заказать',
+	'empty' => 'Корзина пуста',
+	
+	// Controller
+	'added_to_cart' => 'Добавленно а корзину',
+	'was_deleted' => 'Удаленно с корзины',
+	
+
+];
