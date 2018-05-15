7dc13d9fb5a7d696e7aee5e0179234bf3b57e612
Serhii Cho <1990serzhil@gmail.com>
2018-05-15 16:23:41 +0300

    Added cart translation on English

---
+++ resources/lang/en/cart.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+	// View
+	'cart' => 'Cart',
+	'add_or_delete' => 'Save | Delete',
+	'product' => 'Product',
+	'price' => 'Price',
+	'total' => 'Total',
+	'delete' => 'Delete',
+	'order' => 'Order',
+	'empty' => 'Cart is empty',
+	'favorite' => 'Favorite',
+	'add_to_cart' => 'Add to cart',
+	'favorite' => 'Favorite',
+	'add_to_favorite' => 'Add to favorite',
+	'favorite_empty' => 'There are no favorite items',
+	
+	// Controller
+	'added_to_cart' => ':item added to cart',
+	'was_deleted' => 'Removed frome cart',
+	'was_deleted_from_favs' => 'Removed from favorite',
+	'item_is_in_your_cart' => ':item is already in your cart',
+	'added_to_favorite' => ':item added to favorite',
+	'item_is_in_favorite' => 'This item is already added to favorite',
+	'item_is_in_your_cart' => 'Thid item is already in your cart',
+	'moved_to_cart' => ':item moved to cart',
+
+];
