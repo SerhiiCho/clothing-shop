a1ad64a1a4d3be193d38b7506fd97eb804def82d
Serhii Cho <1990serzhil@gmail.com>
2018-05-17 16:53:24 +0300

    Added translation for logs

---
+++ resources/lang/en/logs.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+	'logs' => 'Logs',
+	'more_than_50_mb' => 'The file is more than 50 Mb, please download it',
+	'level' => 'Level',
+	'context' => 'Context',
+	'date' => 'Date',
+	'content' => 'Content',
+	'download' => 'Download',
+	'delete_all_files' => 'Delete all',
+	'delete' => 'Delete',
+	'are_u_sure' => 'Are you sure?',
+	'description' => 'This page is showing logs of all errors and alerts about the website. Access to this page is only for administration.',
+
+];
