5f7ce400481cfb6d671c80afcbdf902dd1f1deed
Serhii Cho <1990serzhil@gmail.com>
2018-05-22 19:12:26 +0300

    Added padding

--- resources/assets/js/components/Popular.vue
+++ resources/assets/js/components/Popular.vue
@@ -1,7 +1,7 @@
 <template>
 	<section class="main-content">
-		<h3 class="display-4  text-center p-4" style="background:#F2F2F2;">{{ popular }}</h3>
-		<div class="container">
+		<h3 class="display-4 text-center p-4" style="background:#F2F2F2;">{{ popular }}</h3>
+		<div class="container pb-3">
 			<section class="row">
 				<div v-for="popular in populars" v-bind:key="popular.id" class="col-lg-2 col-md-3 col-6 col-sm-4 item-card">
 					<a :href="'/item/' + popular.id" :title="popular.title">
