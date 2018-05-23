cb062d54b0dc446c97eed210f3c64c626f50cfb7
Serhii Cho <1990serzhil@gmail.com>
2018-05-23 16:40:11 +0300

    Changed requests files

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -15,33 +15,44 @@ class StoreItemRequest extends FormRequest
     // Get the validation rules that apply to the request.
     public function rules()
     {
-        return [
-            'title'		 => 'required|min:4|max:80',
-            'content'    => 'required|min:4|max:3000',
-            'category'	 => 'required',
-            'type' 		 => 'required',
-            'price' 	 => 'required|numeric',
-            'image'		 => 'required|image|nullable|max:1999'
-        ];
+		$rules = [
+			'title' => 'required|min:4|max:80',
+			'content' => 'required|min:4|max:3000',
+			'category' => 'required',
+			'type' => 'required',
+			'price' => 'required|numeric'
+		];
+
+		if (request()->hasFile('photos')) {
+			$photos = count(request('photos'));
+
+			foreach(range(0, $photos) as $i) {
+				$rules['photos.' . $i] = 'image|nullable|max:1999';
+			}
+		} else {
+			$rules['photos'] = 'required';
+		}
+
+        return $rules;
 	}
 
 	// Custom messages
 	public function messages()
     {
         return [
-            'title.required' 		=> trans('items.title_required'),
-            'content.required' 		=> trans('items.content_required'),
-            'category.required' 	=> trans('items.category_required'),
-            'type.required' 		=> trans('items.type_required'),
-            'image.required'	    => trans('items.image_required'),
-            'price.required' 		=> trans('items.price_required'),
-            'title.min' 			=> trans('items.title_min'),
-            'content.min'			=> trans('items.content_min'),
-            'title.max'				=> trans('items.title_max'),
-            'content.max'			=> trans('items.content_max'),
-            'price.numeric'		=> trans('items.price_numeric'),
-            'image.image'			=> trans('items.image_image'),
-            'image.max'				=> trans('items.image_max')
+            'title.required' => trans('items.title_required'),
+            'content.required' => trans('items.content_required'),
+            'category.required' => trans('items.category_required'),
+            'type.required' => trans('items.type_required'),
+            'photos.required' => trans('items.image_required'),
+            'price.required' => trans('items.price_required'),
+            'title.min' => trans('items.title_min'),
+            'content.min' => trans('items.content_min'),
+            'title.max' => trans('items.title_max'),
+            'content.max' => trans('items.content_max'),
+            'price.numeric' => trans('items.price_numeric'),
+            'photos.image' => trans('items.image_image'),
+            'photos.max' => trans('items.image_max')
         ];
     }
 }
--- app/Http/Requests/UpdateItemRequest.php
+++ app/Http/Requests/UpdateItemRequest.php
@@ -16,11 +16,11 @@ class UpdateItemRequest extends FormRequest
     public function rules()
     {
 		return [
-			'title'		 => 'required|min:4|max:80',
-			'content'    => 'required|min:4|max:3000',
-			'category'	 => 'required',
-			'type' 		 => 'required',
-			'price' 	 => 'required|numeric',
+			'title' => 'required|min:4|max:80',
+			'content' => 'required|min:4|max:3000',
+			'category' => 'required',
+			'type' => 'required',
+			'price' => 'required|numeric',
 		];
 	}
 
@@ -28,17 +28,16 @@ class UpdateItemRequest extends FormRequest
 	public function messages()
     {
         return [
-            'title.required' 		=> trans('items.title_required'),
-            'content.required' 		=> trans('items.content_required'),
-            'category.required' 	=> trans('items.category_required'),
-            'type.required' 		=> trans('items.type_required'),
-            'image.required'	    => trans('items.image_required'),
-            'price.required' 		=> trans('items.price_required'),
-            'title.min' 			=> trans('items.title_min'),
-            'content.min'			=> trans('items.content_min'),
-            'title.max'				=> trans('items.title_max'),
-            'content.max'			=> trans('items.content_max'),
-            'price.numeric'		=> trans('items.price_numeric'),
+            'title.required' => trans('items.title_required'),
+            'content.required' => trans('items.content_required'),
+            'category.required' => trans('items.category_required'),
+            'type.required' => trans('items.type_required'),
+            'price.required' => trans('items.price_required'),
+            'title.min' => trans('items.title_min'),
+            'content.min' => trans('items.content_min'),
+            'title.max' => trans('items.title_max'),
+            'content.max' => trans('items.content_max'),
+            'price.numeric' => trans('items.price_numeric'),
         ];
     }
 }
