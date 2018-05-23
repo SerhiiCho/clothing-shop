0cdb4f89ca940e1015c6aa31788d6370a9451c99
Serhii Cho <1990serzhil@gmail.com>
2018-05-23 19:38:27 +0300

    Fixed bug on slider

--- resources/assets/js/components/Slider.vue
+++ resources/assets/js/components/Slider.vue
@@ -2,14 +2,7 @@
     <div class="main-banner">
 		<div @click="prev" class="imgbanbtn imgbanbtn-prev" style="background-image: url(storage/img/arrow-left.png);"></div>
 
-			<div :style="'background-image:url(storage/img/slider/' + images[Math.abs(currentNumber) % images.length] + ');'" v-on:mouseover="stopRotation" v-on:mouseout="startRotation" class="slider">
-				<!-- <img
-					:src="'/storage/img/slider/' + images[Math.abs(currentNumber) % images.length]"
-					v-on:mouseover="stopRotation"
-					v-on:mouseout="startRotation"
-					alt="banner"
-				/> -->
-			</div>
+			<div v-if="images.length > 0" :style="'background-image:url(storage/img/slider/' + images[Math.abs(currentNumber) % images.length] + ');'" v-on:mouseover="stopRotation" v-on:mouseout="startRotation" class="slider"></div>
 
 		<div @click="next" class="imgbanbtn imgbanbtn-next" style="background-image: url(storage/img/arrow-right.png);"></div>
 	</div>
