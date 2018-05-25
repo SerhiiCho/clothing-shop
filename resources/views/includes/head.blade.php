40fd1a1fd56dd61900e30c31e7c3a59f71469a96
Serhii Cho <1990serzhil@gmail.com>
2018-05-25 19:02:03 +0300

    renamed one line in head

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,9 +1,13 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
+
 <title>@yield('title') | {{ config('app.name', 'Clothing Shop') }}</title>
-{!! styleTimestamp('/css/bootstrap.min.css') !!}
+
+{!! styleTimestamp('/css/bootstrap.css') !!}
 {!! styleTimestamp('/css/app.css') !!}
+
 <link rel="shortcut icon" href="{{ asset('storage/img/favicon.png') }}" type="image/x-icon">
