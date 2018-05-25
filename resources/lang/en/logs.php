9ee243d8eb3d74ba58897a00e9983972bb070c95
Serhii Cho <1990serzhil@gmail.com>
2018-05-25 18:58:57 +0300

    - one line in lang file: logs

--- resources/lang/en/logs.php
+++ resources/lang/en/logs.php
@@ -12,6 +12,5 @@ return [
 	'delete_all_files' => 'Delete all',
 	'delete' => 'Delete',
 	'are_u_sure' => 'Are you sure?',
-	'description' => 'This page is showing logs of all errors and alerts about the website. Access to this page is only for administration.',
 
 ];
