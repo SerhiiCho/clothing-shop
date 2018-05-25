e07f3fc9e3e4405fbe7401923791498a4a49b37d
Serhii Cho <1990serzhil@gmail.com>
2018-05-25 14:06:55 +0300

    Fixed cart link on mobile screen

--- resources/views/includes/cart.blade.php
+++ resources/views/includes/cart.blade.php
@@ -1,10 +1,11 @@
-<a href="/cart" class="text-dark d-inline mt-1 cart">
+<a href="/cart" class="text-dark mt-1 cart">
 	<i class="fa fa-shopping-cart" aria-hidden="true"></i>  
 	@lang('cart.cart')
+	
+	@if (Cart::instance('default')->count() > 0)
+		<span class="text-primary text-bold d-inline">
+			{{ Cart::instance('default')->count() }}
+		</span>
+	@endif
 </a>
 
-@if (Cart::instance('default')->count() > 0)
-	<span class="text-primary text-bold d-inline">
-		{{ Cart::instance('default')->count() }}
-	</span>
-@endif
