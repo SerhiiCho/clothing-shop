e0342733cca7e4bddcb19d954d45051912708fb0
Serhii Cho <1990serzhil@gmail.com>
2018-05-25 19:00:09 +0300

    - if statement in includes/cart

--- resources/views/includes/cart.blade.php
+++ resources/views/includes/cart.blade.php
@@ -1,12 +1,10 @@
-@if (Cart::count() + Cart::instance('favorite')->count() > 0)
-	<a href="/cart" class="text-dark mt-1 cart">
-		<i class="fa fa-shopping-cart" aria-hidden="true"></i>  
-		@lang('cart.cart')
-		
-		@if (Cart::instance('default')->count() > 0)
-			<span class="text-primary text-bold d-inline">
-				{{ Cart::instance('default')->count() }}
-			</span>
-		@endif
-	</a>
-@endif
+<a href="/cart" class="text-dark mt-1 cart">
+	<i class="fa fa-shopping-cart" aria-hidden="true"></i>  
+	@lang('cart.cart')
+	
+	@if (Cart::instance('default')->count() > 0)
+		<span class="text-success text-bold d-inline">
+			{{ Cart::instance('default')->count() }}
+		</span>
+	@endif
+</a>
