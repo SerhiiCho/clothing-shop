a8a1f8aac4c96cc6ecdee006159c795b35987463
Serhii Cho <1990serzhil@gmail.com>
2018-05-28 18:20:11 +0300

    + ItemHelpers file and created some functions to clean controller In item controller cleaned  lots of stuff

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use App\Models\Type;
-use App\Models\ItemsPhoto;
 use Illuminate\Http\Request;
-use Intervention\Image\ImageManager;
+use App\Contracts\ItemHelpers;
 use Illuminate\Support\Facades\Cookie;
 use App\Http\Requests\StoreItemRequest;
 use Illuminate\Support\Facades\Storage;
@@ -14,6 +13,8 @@ use App\Http\Requests\UpdateItemRequest;
 
 class ItemController extends Controller
 {
+	use ItemHelpers;
+
 	public function __construct()
     {
 		$this->middleware('member')->only(['create', 'store']);
@@ -21,16 +22,11 @@ class ItemController extends Controller
 	}
 	
     public function index()
-    {
-		$current_category = whatIsCurrent('category');
-
-		if ($current_category === 'women' || $current_category === 'men') {
-			$sidebar = true;
-		} else {
-			$sidebar = false;
-		}
-
-        return view('items.index')->with(compact('current_category', 'sidebar'));
+    {	
+        return view('items.index')->with([
+			'current_category' => whatIsCurrent('category'),
+			'sidebar' => $this->currentStateOfSidebar( whatIsCurrent('category') )
+		]);
 	}
 
     public function create()
@@ -42,33 +38,8 @@ class ItemController extends Controller
 
     public function store(StoreItemRequest $request)
     {
-		$item = auth()->user()->items()->create([
-			'title' => $request->title,
-			'content' => $request->content,
-			'category' => $request->category,
-			'price' => $request->price,
-			'type_id' => $request->type
-		]);
-
-			
-		$i = 0;
-
-        foreach ($request->photos as $image) {
-			$i++;
-			if ($i <= 5) {
-				$ext = $image->getClientOriginalExtension();
-				$filename = getFileName($request->title, $ext);
-
-				(new ImageManager)->make($image)->resize(451, 676)->save(
-					storage_path('app/public/img/clothes/' . $filename
-				));
-
-				ItemsPhoto::create([
-					'item_id' => $item->id,
-					'name' => $filename
-				]);
-			}
-		}
+		$item = $this->createOrUpdateItem($request);
+		$this->uploadPhotos($request, $item->id);
 
 		return redirect('items')->withSuccess(
 			trans('items.item_added')
@@ -78,14 +49,14 @@ class ItemController extends Controller
 
     public function show($category, Item $item)
     {
-		if (Cookie::get('visited')) {
-			if (Cookie::get('visited') == $item->id) {
-				return view('items.show')->with([
-					'item_id' 	 => $item->id,
-					'item_title' => $item->title
-				]);
-			}
+		// If page has been just visited just return view
+		if (Cookie::get('visited') && Cookie::get('visited') == $item->id) {
+			return view('items.show')->with([
+				'item_id' 	 => $item->id,
+				'item_title' => $item->title
+			]);
 		}
+
 		Cookie::queue('visited', $item->id, 1);
 		$item->increment('popular');
 
@@ -116,35 +87,10 @@ class ItemController extends Controller
 				Storage::delete('public/img/clothes/' . $photo->name);
 				$photo->delete();
 			}
-			$i = 0;
-
-			foreach ($request->photos as $image) {
-				if ($i <= 5) {
-					$ext = $image->getClientOriginalExtension();
-					$filename = getFileName($request->title, $ext);
-		
-					(new ImageManager)->make($image)->resize(451, 676)->save(
-						storage_path('app/public/img/clothes/' . $filename
-					));
-
-					ItemsPhoto::create([
-						'item_id' => $item->id,
-						'name' => $filename
-					]);
-				}
-			}
+			$this->uploadPhotos($request, $item->id);
 		}
+		$this->createOrUpdateItem($request, $item);
 
-		$item->update([
-			'title' => $request->title,
-			'content' => $request->content,
-			'category' => $request->category,
-			'price' => $request->price,
-			'type_id' => $request->type
-		]);
-
-		return back()->withSuccess(
-			trans('items.item_changed')
-		);
+		return back()->withSuccess(trans('items.item_changed'));
     }
 }
