14b2a171ce12214a3c2d3d5ad35c3b6dd354ad16
Serhii Cho <1990serzhil@gmail.com>
2018-05-29 11:59:28 +0300

    + pivot table item_message

---
+++ database/migrations/2018_05_28_185657_create_table_item_message.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableItemMessage extends Migration
+{
+    public function up()
+    {
+        Schema::create('item_message', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('item_id')->unsigned();
+			$table->foreign('item_id')->references('id')->on('items');
+			$table->integer('message_id')->unsigned();
+            $table->foreign('message_id')->references('id')->on('messages');
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('item_message');
+    }
+}
