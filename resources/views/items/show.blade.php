d6d24bc716f1e6e7e0cf9aa4d0f160e8ae24dca5
Serhii Cho <1990serzhil@gmail.com>
2018-05-29 14:30:16 +0300

    + some incoded data to items/show .blade.php

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -7,12 +7,14 @@
 <div class="single-container container">
 	<single-item
 		:admin={{ json_encode(optional(auth()->user())->admin) }}
-		:number-item="{{ json_encode(trans('items.number_item')) }}"
+		:code-of-the-item="{{ json_encode(trans('items.code_of_the_item')) }}"
 		:hryvnia="{{ json_encode(trans('items.hryvnia')) }}"
 		:add-to-cart="{{ json_encode(trans('items.add_to_cart')) }}"
 		:deleting="{{ json_encode(trans('items.delete')) }}"
 		:change="{{ json_encode(trans('items.change')) }}"
 		:token="{{ json_encode(csrf_token()) }}"
+		:all-amount1="{{ json_encode(trans('items.all_amount_1')) }}"
+		:all-amount2="{{ json_encode(trans('items.all_amount_2')) }}"
 		:added-to-cart="{{ json_encode(trans('items.added_to_cart')) }}"
 		:you-already-send-order="{{ json_encode(trans('items.you_already_send_order')) }}"
 		:delete-this-product="{{ json_encode(trans('items.delete_this_product')) }}"
