1637f2be290ad7bfaa0aa3c47a831b248a52fbab
Serhii Cho <1990serzhil@gmail.com>
2018-05-29 12:06:10 +0300

    + 2 resource files for messages

---
+++ app/Http/Resources/ItemLightResource.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ItemLightResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+			'id' => $this->id,
+			'title' => $this->title,
+			'category' => $this->category
+		];
+    }
+}
---
+++ app/Http/Resources/MessageResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\LightItemResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+			'id' => $this->id,
+			'ip' => $this->ip,
+			'phone' => $this->phone,
+			'name' => $this->name,
+			'total' => $this->total,
+			'order' => $this->order,
+			'created_at' => $this->created_at,
+			'items' => ItemLightResource::collection($this->items)
+		];
+    }
+}
