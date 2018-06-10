05285b9e47dc7d3930604bbcc4cd24235febddbb
Serhii <35465417+SerhiiCho@users.noreply.github.com>
2018-06-10 19:38:56 +0300

    Update script.blade.php
    Deleted defer from script tags

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,2 +1,2 @@
-<script src="{{ asset('js/app.js') }}" defer></script>
-<script src="{{ asset('js/main.js') }}" defer></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
