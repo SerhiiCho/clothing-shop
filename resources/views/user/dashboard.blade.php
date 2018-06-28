45ba4819d9e10148679444d04bf46ecf64c9be4b
Serhii Cho <1990serzhil@gmail.com>
2018-06-28 18:28:07 +0300

    Changed dashboard blade

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -3,48 +3,8 @@
 @section('title', auth()->user()->name)
 
 @section('content')
-<div class="container pt-3 pb-5">
-	<h4 class="display-4 text-center">{{ user()->name }}</h4>
-	<p class="text-center mb-3">@lang('profile.your_rights')</p>
-	<div class="row">
-		<div class="col-md-6 offset-md-3">
-			{{-- Member --}}
-			<div class="list-group">
-				<a class="list-group-item list-group-item-{{ user()->isMember() ? 'success' : 'danger' }}">
-					@lang('profile.member') 
-					<span class="pull-right">
-						<i class="fa fa-{{ user()->isMember() ? 'check' : 'times' }}" aria-hidden="true"></i>
-					</span>
-				</a>
-			</div>
-			{{-- Admin --}}
-			<div class="list-group">
-				<a class="list-group-item list-group-item-{{ user()->isAdmin() ? 'success' : 'danger' }}">
-					@lang('profile.admin') 
-					<span class="pull-right">
-						<i class="fa fa-{{ user()->isAdmin() ? 'check' : 'times' }}" aria-hidden="true"></i>
-					</span>
-				</a>
-			</div>
-			{{-- Blogger --}}
-			<div class="list-group">
-				<a class="list-group-item list-group-item-{{ user()->isBlogger() ? 'success' : 'danger' }}">
-					@lang('profile.blogger') 
-					<span class="pull-right">
-						<i class="fa fa-{{ user()->isBlogger() ? 'check' : 'times' }}" aria-hidden="true"></i>
-					</span>
-				</a>
-			</div>
-			{{-- Master --}}
-			<div class="list-group">
-				<a class="list-group-item list-group-item-{{ user()->isMaster() ? 'success' : 'danger' }}">
-					@lang('profile.master') 
-					<span class="pull-right">
-						<i class="fa fa-{{ user()->isMaster() ? 'check' : 'times' }}" aria-hidden="true"></i>
-					</span>
-				</a>
-			</div>
-		</div>
-	</div>
+<div class="container pt-3 pb-5 text-center">
+	<h3>@lang('messages.smile') :)</h3>
+	<h5>@lang('messages.all_items', ['items' => $all_items])</h5>
 </div>
 @endsection
