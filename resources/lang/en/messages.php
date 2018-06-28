11d6d02fdc2647d386da9408b8876050955e9316
Serhii Cho <1990serzhil@gmail.com>
2018-06-28 18:27:52 +0300

    + messages to messages.php lang files

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,7 +5,9 @@ return [
 	// Views
 	'more_clothes' => 'More clothes',
 	'welcome' => 'Welcome',
+	'all_items' => 'All products :items items',
 	'back' => 'Back',
+	'smile' => 'Smile',
 	'have_no_rights' => 'Access to this page is opened only for registered users with certain rights',
 
 ];
