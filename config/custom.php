29307648f7a8b7ae81ca34b00e431740e19c6e42
Serhii Cho <1990serzhil@gmail.com>
2018-06-29 10:57:08 +0300

    Added custom passwords to custom config file

--- config/custom.php
+++ config/custom.php
@@ -6,6 +6,10 @@ return [
 	'url_key' => env('URL_KEY'),
 
 	// Enter the auth paths with this slug
-	'enter_slug' => env('ENTER_SLUG')
+	'enter_slug' => env('ENTER_SLUG'),
+
+	// Passwords for users
+	'pwd1' => env('PWD1'),
+	'pwd2' => env('PWD2')
 
 ];
