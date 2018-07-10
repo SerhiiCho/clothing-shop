b5c8f2cc237324ea42ce45d4174cea96c98be478
Serhii Cho <1990serzhil@gmail.com>
2018-07-10 15:06:35 +0300

    Добавил Event and Listener

---
+++ app/Listeners/SendSmsListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\RecivedOrdeEvent;
+
+class SendSmsListener
+{
+    public function handle(RecivedOrdeEvent $event)
+    {
+        $client = new \Nexmo\Client(
+			new \Nexmo\Client\Credentials\Basic(
+				config('services.nexmo.key'),
+				config('services.nexmo.secret')
+			)
+		);
+
+		$client->message()->send([
+			'type' => 'unicode',
+			'from' => config('services.nexmo.sms_from'),
+			'to' => config('services.nexmo.sms_to'),
+			'text' => 'Новый заказ от ' . $event->phone . ' |'
+		]);
+    }
+}
