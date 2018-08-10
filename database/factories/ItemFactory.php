dac236d75f87dec4d0bdda0aaad2ac6e142ecb9a
Serhii Cho <1990serzhil@gmail.com>
2018-08-10 18:39:24 +0300

    changed 2 factories

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -6,12 +6,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Item::class, function (Faker $faker) {
     return [
-		'title' => $faker->name,
-		'content' => $faker->paragraph(7),
-		'category' => rand(1, 2) == 1 ? 'women' : 'men',
-		'stock' => rand(1, 4),
-		'price' => rand(200, 1500),
-		'user_id' => 1,
-		'type_id' => rand(1, Type::count())
+        'title' => $faker->name,
+        'content' => $faker->paragraph(7),
+        'category' => rand(1, 2) == 1 ? 'women' : 'men',
+        'stock' => rand(1, 4),
+        'price' => rand(200, 1500),
+        'user_id' => 1,
+        'type_id' => rand(1, Type::count()),
     ];
 });
--- database/factories/ItemsPhotoFactory.php
+++ database/factories/ItemsPhotoFactory.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Models\Item;
 use App\Models\ItemsPhoto;
 use Faker\Generator as Faker;
 
 $factory->define(ItemsPhoto::class, function (Faker $faker) {
-    return ['name' => 'default.jpg'];
+    return [
+        'name' => 'default.jpg',
+        'item_id' => factory(Item::class)->create(),
+    ];
 });
