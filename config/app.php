00ce2046eb2dfe3146489f4c0ceeeeecb7fe0b57
SerhiiCho <serhiicho@inbox.ru>
2018-08-29 16:07:18 +0300

    ..

--- config/app.php
+++ config/app.php
@@ -74,7 +74,6 @@ return [
         App\Providers\EloquentEventProvider::class,
         App\Providers\GsmProvider::class,
         App\Providers\BladeProvider::class,
-        App\Providers\DatabaseProvider::class,
         App\Providers\HeaderProvider::class,
 
     ],
