25797bcd3fc9fb30f14b261d6d278d6f7f29b6a5
SerhiiCho <serhiicho@inbox.ru>
2018-12-21 17:45:38 +0200

    -0- Changed mistakes

--- resources/lang/ru/cart.php
+++ resources/lang/ru/cart.php
@@ -2,29 +2,29 @@
 
 return [
 
-	// View
-	'cart' => 'Корзина',
-	'add_or_delete' => 'Сохранить | Удалить',
-	'product' => 'Продукт',
-	'price' => 'Цена',
-	'total' => 'Всего',
-	'delete' => 'Удалить',
-	'order' => 'Заказать',
-	'empty' => 'Ваша корзина пуста',
-	'favorite' => 'Избранное',
-	'add_to_cart' => 'Добавить в корзину',
-	'favorite' => 'Избранное',
-	'add_to_favorite' => 'Добавить в избранное',
-	'favorite_empty' => 'Нет избранных вещей',
-	
-	// Controller
-	'added_to_cart' => ':item добавленно а корзину',
-	'was_deleted' => 'Удалено с корзины',
-	'was_deleted_from_favs' => 'Удалено с избранного',
-	'item_is_in_your_cart' => ':item уже в вашей корзине',
-	'added_to_favorite' => ':item добавлен в избранное',
-	'item_is_in_favorite' => 'Этот товар уже добавлен в избранное',
-	'item_is_in_your_cart' => 'Этот товар уже в корзине',
-	'moved_to_cart' => ':item перенесен в корзину',
+    // View
+    'cart' => 'Корзина',
+    'add_or_delete' => 'Сохранить | Удалить',
+    'product' => 'Продукт',
+    'price' => 'Цена',
+    'total' => 'Всего',
+    'delete' => 'Удалить',
+    'order' => 'Заказать',
+    'empty' => 'Ваша корзина пуста',
+    'favorite' => 'Избранное',
+    'add_to_cart' => 'Добавить в корзину',
+    'favorite' => 'Избранное',
+    'add_to_favorite' => 'Добавить в избранное',
+    'favorite_empty' => 'Нет избранных вещей',
+
+    // Controller
+    'added_to_cart' => ':item добавлено а корзину',
+    'was_deleted' => 'Удалено с корзины',
+    'was_deleted_from_favs' => 'Удалено с избранного',
+    'item_is_in_your_cart' => ':item уже в вашей корзине',
+    'added_to_favorite' => ':item добавлен в избранное',
+    'item_is_in_favorite' => 'Этот товар уже добавлен в избранное',
+    'item_is_in_your_cart' => 'Этот товар уже в корзине',
+    'moved_to_cart' => ':item перенесен в корзину',
 
 ];
--- resources/lang/ru/forms.php
+++ resources/lang/ru/forms.php
@@ -2,16 +2,16 @@
 
 return [
 
-	'login_title' => 'Войти',
-	'register_title' => 'Регистрация',
-	'forgot_password' => 'Забыли пароль',
-	'input_email_address' => 'Эл. адресс',
-	'input_password' => 'Пароль',
-	'input_remember_me' => 'Запомнить меня',
-	'input_name' => 'Имя',
-	'confirm_password' => 'Подтвердите пароль',
-	'choose_file' => 'Выбрать файл',
-	'choose_type' => 'Выберите тип',
-	'choose_category' => 'Выберите категорию',
+    'login_title' => 'Войти',
+    'register_title' => 'Регистрация',
+    'forgot_password' => 'Забыли пароль',
+    'input_email_address' => 'Эл. адрес',
+    'input_password' => 'Пароль',
+    'input_remember_me' => 'Запомнить меня',
+    'input_name' => 'Имя',
+    'confirm_password' => 'Подтвердите пароль',
+    'choose_file' => 'Выбрать файл',
+    'choose_type' => 'Выберите тип',
+    'choose_category' => 'Выберите категорию',
 
 ];
