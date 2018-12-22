2e23d40310aab7a426d80d2bedd3d3094da934c1
SerhiiCho <serhiicho@inbox.ru>
2018-12-22 12:14:22 +0200

    -1- Added 3 test cases for Slider view

---
+++ tests/Feature/Views/Slider/SliderCreatePageTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Views\Slider;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class SliderCreatePageTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_not_accessable_by_auth(): void
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->get("/slider/create")
+            ->assertRedirect();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_not_accessable_by_guest(): void
+    {
+        $this->get("/slider/create")
+            ->assertRedirect();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_accessable_by_admin(): void
+    {
+        $this->actingAs(factory(User::class)->state('admin')->create())
+            ->get("/slider/create")
+            ->assertOk()
+            ->assertViewIs('slider.create');
+    }
+}
---
+++ tests/Feature/Views/Slider/SliderEditPageTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Views\Slider;
+
+use App\Models\Slider;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class SliderEditPageTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $slider;
+
+    /**
+     * @author Cho
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->slider = factory(Slider::class)->create();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_not_accessable_by_auth(): void
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->get("/slider/{$this->slider->id}/edit")
+            ->assertRedirect();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_not_accessable_by_guest(): void
+    {
+        $this->get("/slider/{$this->slider->id}/edit")
+            ->assertRedirect();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_accessable_by_admin(): void
+    {
+        $this->actingAs(factory(User::class)->state('admin')->create())
+            ->get("/slider/{$this->slider->id}/edit")
+            ->assertOk()
+            ->assertViewIs('slider.edit');
+    }
+}
