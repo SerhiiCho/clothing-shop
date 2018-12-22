275256f61834654751a93f78c875b8569c5b038b
SerhiiCho <serhiicho@inbox.ru>
2018-12-22 19:56:39 +0200

    -1- Moved CardController to Admin folder and added doc blocks to methods

--- app/Http/Controllers/Admin/CardController.php
+++ app/Http/Controllers/Admin/CardController.php
@@ -1,40 +1,59 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCardRequest;
 use App\Http\Requests\UpdateCardRequest;
 use App\Models\Card;
 use App\Models\Type;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
 use Intervention\Image\ImageManager;
 
 class CardController extends Controller
 {
+    /**
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('admin');
     }
 
-    // Display a listing of the resource
-    public function index()
+    /**
+     * Display all cards
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(): View
     {
         return view('cards.index', [
             'cards' => Card::get(),
         ]);
     }
 
-    // Show the form for creating a new resource
-    public function create()
+    /**
+     * Display create card page with form
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create(): View
     {
         return view('cards.create', [
             'types' => Type::orderBy('name')->get(),
         ]);
     }
 
-    // Store a newly created resource in storage
-    public function store(StoreCardRequest $request)
+    /**
+     * Store new card in database
+     *
+     * @param \App\Http\Requests\StoreCardRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(StoreCardRequest $request): RedirectResponse
     {
         if (Card::count() >= 3) {
             return back()->withError(trans('cards.already_3_cards'));
@@ -44,7 +63,7 @@ class CardController extends Controller
         $ext = $image->getClientOriginalExtension();
         $filename = getFileName($request->type, $ext);
 
-        $this->makeCardImage($image, $filename);
+        $this->uploadCardImage($image, $filename);
 
         Card::create([
             'image' => $filename,
@@ -59,8 +78,13 @@ class CardController extends Controller
         );
     }
 
-    // Show the form for editing the specified resource.
-    public function edit(Card $card)
+    /**
+     * Display edit card page with form
+     *
+     * @param \App\Models\Card $card
+     * @return \Illuminate\View\View
+     */
+    public function edit(Card $card): View
     {
         $types = Type::orderBy('name')->get();
 
@@ -69,8 +93,14 @@ class CardController extends Controller
         );
     }
 
-    // Update the specified resource in storage
-    public function update(UpdateCardRequest $request, Card $card)
+    /**
+     * Update card in database
+     *
+     * @param \App\Http\Requests\UpdateCardRequest $request
+     * @param \App\Models\Card $card
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UpdateCardRequest $request, Card $card): RedirectResponse
     {
         if ($request->hasFile('image')) {
             if ($card->image != 'default.jpg') {
@@ -80,7 +110,7 @@ class CardController extends Controller
             $ext = $image->getClientOriginalExtension();
             $filename = getFileName($request->type, $ext);
 
-            $this->makeCardImage($image, $filename);
+            $this->uploadCardImage($image, $filename);
 
             $card->update(['image' => $filename]);
         }
@@ -101,8 +131,11 @@ class CardController extends Controller
      * Remove the specified resource from storage,
      * App\Observers\CardObserver will delete image
      * file while database record is being deleted
+     *
+     * @param \App\Models\Card $card
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy(Card $card)
+    public function destroy(Card $card): RedirectResponse
     {
         cache()->forget('home_cards');
 
@@ -112,11 +145,13 @@ class CardController extends Controller
     }
 
     /**
+     * Method helper upload image in storage
+     *
      * @param \Illuminate\Http\UploadedFile $image_inst
      * @param string $filename
      * @return void
      */
-    public function makeCardImage(UploadedFile $image_inst, string $filename): void
+    public function uploadCardImage(UploadedFile $image_inst, string $filename): void
     {
         (new ImageManager)
             ->make($image_inst)
