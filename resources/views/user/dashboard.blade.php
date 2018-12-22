1e0491f9993d1f6f163c81e8903eeadfebb7a035
SerhiiCho <serhiicho@inbox.ru>
2018-12-22 14:56:28 +0200

    -1- Added bootstrap table to dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,7 +4,14 @@
 
 @section('content')
 <div class="container pt-3 pb-5 text-center">
-    <h3>{{ user()->name }}</h3>
-    <h5>@lang('messages.all_items', ['items' => $all_items])</h5>
+    <h4 class="mb-3">@lang('messages.stats')</h4>
+    <table class="table table-hover">
+        <tbody>
+            <tr>
+                <td>@lang('messages.all_items'):</td>
+                <td>{{ $all_items }}</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 @endsection
