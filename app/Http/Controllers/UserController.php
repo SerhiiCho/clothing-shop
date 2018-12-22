b187e0ba83cf2c32fcd485b8f2c3e8e2d69211cd
SerhiiCho <serhiicho@inbox.ru>
2018-12-22 18:47:27 +0200

    -0- In user controller chaned middleware from auth to admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('admin');
     }
 
     public function dashboard()
