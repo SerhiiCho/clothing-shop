08670816a5e827f107e93270a7e45ebb8a5c79e4
SerhiiCho <serhiicho@inbox.ru>
2018-12-22 12:08:43 +0200

    -1- Added tests for Cards Views

--- database/factories/CardFactory.php
+++ database/factories/CardFactory.php
@@ -2,9 +2,8 @@
 
 use App\Models\Card;
 use App\Models\Type;
-use Faker\Generator as Faker;
 
-$factory->define(Card::class, function (Faker $faker) {
+$factory->define(Card::class, function () {
     return [
         'type_id' => rand(1, Type::count()),
         'category' => rand(1, 2) == 1 ? 'women' : 'men',
---
+++ tests/Feature/Views/Cards/CardsIndexPageTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Views\Cards;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CardsIndexPageTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_not_accessable_by_guest(): void
+    {
+        $this->get('/cards')
+            ->assertRedirect();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_not_accessable_by_auth(): void
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->get('/cards')
+            ->assertRedirect();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function page_is_accessable_by_admin(): void
+    {
+        $this->actingAs(factory(User::class)->state('admin')->create())
+            ->get('/cards')
+            ->assertOk()
+            ->assertViewIs('cards.index');
+    }
+}
