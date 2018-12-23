eecb064e4e33e4c4e89d1af91bb03bdebaa60159
SerhiiCho <serhiicho@inbox.ru>
2018-12-23 22:47:43 +0200

    -1- Added global variable $admin_options accross all views to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Option;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,12 +13,32 @@ class AppServiceProvider extends ServiceProvider
     {
         \Schema::defaultStringLength(191);
 
-        // dump_sql();
+        $this->fetchAdminOptionsFromDb();
 
         //\Artisan::call('migrate');
 
         // if (app()->env === 'production') {
         //     \URL::forceScheme('https');
         // }
+    }
+
+    /**
+     * @return void
+     */
+    public function fetchAdminOptionsFromDb(): void
+    {
+        $admin_options = cache()->rememberForever('admin_options', function () {
+            try {
+                $options = Option::get();
+                return [
+                    'registration' => $options->where('option', 'registration')->first()->value,
+                    'men_category' => $options->where('option', 'men_category')->first()->value,
+                    'women_category' => $options->where('option', 'women_category')->first()->value,
+                ];
+            } catch (QueryException $e) {
+                logs()->error($e->getMessage());
+            }
+        });
+        view()->share(compact('admin_options'));
     }
 }
