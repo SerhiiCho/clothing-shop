6d69d47c02f774f1f0b27821c443e2269f65a991
SerhiiCho <serhiicho@inbox.ru>
2018-12-23 19:58:06 +0200

    -1- Added test to RegisterPageTest.php

--- tests/Feature/Views/Auth/RegisterPageTest.php
+++ tests/Feature/Views/Auth/RegisterPageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Views\Auth;
 
+use App\Models\Option;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
@@ -10,13 +11,24 @@ class RegisterPageTest extends TestCase
 {
     use DatabaseTransactions;
 
+    private $url;
+
+    /**
+     * @author Cho
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->url = '/' . config('custom.enter_slug') . '/register';
+    }
+
     /**
      * @author Cho
      * @test
      */
     public function page_is_accessable_by_guest(): void
     {
-        $this->get('/' . config('custom.enter_slug') . '/register')
+        $this->get($this->url)
             ->assertOk()
             ->assertViewIs('auth.register');
     }
@@ -28,7 +40,18 @@ class RegisterPageTest extends TestCase
     public function page_is_not_accessable_by_auth(): void
     {
         $this->actingAs(factory(User::class)->create())
-            ->get('/' . config('custom.enter_slug') . '/register')
+            ->get($this->url)
             ->assertRedirect();
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function register_form_is_hidded_if_in_db_table_options_set_value_to_off(): void
+    {
+        $this->get($this->url)->assertSeeText(trans('forms.input_password'));
+        Option::set('registration', 'off');
+        $this->get($this->url)->assertDontSeeText(trans('forms.input_password'));
     }
 }
