b8eee94b3af3de2615a71057b4a2aaea950e1f0b
SerhiiCho <serhiicho@inbox.ru>
2018-12-24 15:23:52 +0200

    -0-

--- config/cart.php
+++ config/cart.php
@@ -10,7 +10,7 @@ return [
     | This default tax rate will be used when you make a class implement the
     | Taxable interface and use the HasTax trait.
     |
-    */
+     */
 
     'tax' => 0,
 
@@ -22,12 +22,11 @@ return [
     | Here you can set the connection that the shoppingcart should use when
     | storing and restoring a cart.
     |
-    */
+     */
 
     'database' => [
 
         'connection' => null,
-
         'table' => 'shoppingcart',
 
     ],
@@ -40,7 +39,7 @@ return [
     | When this option is set to 'true' the cart will automatically
     | destroy all cart instances when the user logs out.
     |
-    */
+     */
 
     'destroy_on_logout' => false,
 
@@ -52,7 +51,7 @@ return [
     | This defaults will be used for the formated numbers if you don't
     | set them in the method call.
     |
-    */
+     */
 
     'format' => [
 
@@ -60,7 +59,7 @@ return [
 
         'decimal_point' => '.',
 
-        'thousand_seperator' => ' '
+        'thousand_seperator' => ' ',
 
     ],
 
