4e0241e6280af9d60d6352cc6c31f00b7d96f742
SerhiiCho <serhiicho@inbox.ru>
2018-12-26 19:56:02 +0200

    -1- Added tests to ContactRequestTest.php

--- tests/Feature/Requests/ContactRequestTest.php
+++ tests/Feature/Requests/ContactRequestTest.php
@@ -23,7 +23,7 @@ class ConractRequestTest extends TestCase
         $this->admin = factory(User::class)->state('admin')->create();
         $this->form_data = [
             'phone' => '(095) 777-77-' . rand(10, 99),
-            'icon' => rand(1, 4),
+            'icon' => rand(1, 10),
         ];
     }
     /**
@@ -32,8 +32,7 @@ class ConractRequestTest extends TestCase
      */
     public function icon_is_not_required(): void
     {
-        $this->actingAs($this->admin)
-            ->post(action('Admin\ContactController@store'), $this->form_data);
+        $this->makeRequest();
 
         $this->assertDatabaseHas('contacts', [
             'phone' => $this->form_data['phone'],
@@ -48,11 +47,78 @@ class ConractRequestTest extends TestCase
     {
         $this->form_data['icon'] = 'text';
 
-        $this->actingAs($this->admin)
-            ->post(action('Admin\ContactController@store'), $this->form_data);
+        $this->makeRequest();
 
         $this->assertDatabaseMissing('contacts', [
             'phone' => $this->form_data['phone'],
         ]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function icon_must_be_beetween_two_numbers(): void
+    {
+        $this->form_data['icon'] = config('valid.contact.icon.max') + 1;
+        $this->makeRequest();
+        $this->assertDatabaseMissing('contacts', [
+            'phone' => $this->form_data['phone'],
+        ]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function phone_must_have_min_length(): void
+    {
+        $phone_min = config('valid.contact.phone.min');
+
+        $this->form_data['phone'] = str_random($phone_min - 1);
+        $this->makeRequest();
+        $this->assertDatabaseMissing('contacts', [
+            'phone' => $this->form_data['phone'],
+        ]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function phone_must_have_max_length(): void
+    {
+        $phone_max = config('valid.contact.phone.max');
+
+        $this->form_data['phone'] = str_random($phone_max + 1);
+        $this->makeRequest();
+        $this->assertDatabaseMissing('contacts', [
+            'phone' => $this->form_data['phone'],
+        ]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function phone_is_required(): void
+    {
+        $this->form_data['phone'] = '';
+
+        $this->makeRequest();
+        $this->assertDatabaseMissing('contacts', [
+            'phone' => $this->form_data['phone'],
+        ]);
+    }
+
+    /**
+     * Method helper
+     *
+     * @return void
+     */
+    public function makeRequest(): void
+    {
+        $this->actingAs($this->admin)
+            ->post(action('Admin\ContactController@store'), $this->form_data);
     }
 }
