1e8d3e2c2a0a44d8b6eb79b73e165586fefbc96b
SerhiiCho <serhiicho@inbox.ru>
2018-12-26 15:13:06 +0200

    -0- Admin middleware added comment @param to dock-block

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -12,6 +12,7 @@ class AdminMiddleware
      *
      * @param \Illuminate\Http\Request $request
      * @param \Closure $next
+     * @param  string|null  $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
