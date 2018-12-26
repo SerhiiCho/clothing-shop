734fdf1a088cfc3631ad2c0c9515adec1e8da21e
SerhiiCho <serhiicho@inbox.ru>
2018-12-26 23:05:19 +0200

    -1- Imported class namespace that i forgot in SliderController

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSliderRequest;
 use App\Http\Requests\UpdateSliderRequest;
 use App\Models\Slider;
 use Illuminate\Http\RedirectResponse;
