96379985b7bb0e969356dad6dd98e8b3d3d73eec
SerhiiCho <serhiicho@inbox.ru>
2018-12-27 20:58:52 +0200

    -1- Added if statement check for SendSmsListener. If no nexmo data provided, then not make sms request

--- app/Listeners/SendSmsListener.php
+++ app/Listeners/SendSmsListener.php
@@ -8,18 +8,20 @@ class SendSmsListener
 {
     public function handle(RecivedOrdeEvent $event)
     {
-        $client = new \Nexmo\Client(
-            new \Nexmo\Client\Credentials\Basic(
-                config('services.nexmo.key'),
-                config('services.nexmo.secret')
-            )
-        );
+        if (!is_null(config('services.nexmo.secret'))) {
+            $client = new \Nexmo\Client(
+                new \Nexmo\Client\Credentials\Basic(
+                    config('services.nexmo.key'),
+                    config('services.nexmo.secret')
+                )
+            );
 
-        $client->message()->send([
-            'type' => 'unicode',
-            'from' => config('services.nexmo.sms_from'),
-            'to' => config('services.nexmo.sms_to'),
-            'text' => 'Новый заказ от ' . $event->phone . ' |',
-        ]);
+            $client->message()->send([
+                'type' => 'unicode',
+                'from' => config('services.nexmo.sms_from'),
+                'to' => config('services.nexmo.sms_to'),
+                'text' => 'Новый заказ от ' . $event->phone . ' |',
+            ]);
+        }
     }
 }
