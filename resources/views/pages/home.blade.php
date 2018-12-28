0a648429ab4f8d3e2fcd3deb53670296fc49a165
SerhiiCho <serhiicho@inbox.ru>
2018-12-28 19:50:46 +0200

    -1- Moved home sections to their own include file

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -39,73 +39,26 @@
         </div>
     @endif
 
+    {{-- Home Up Section --}}
+    @isset($home_sections)
+        @include('includes.section', [
+            'section' => $home_sections[0],
+            'anchor' => 'home_up'
+        ])
+    @endisset
+
     <items-api
         hryvnia="@lang('items.hryvnia')"
         headline="@lang('cards.popular')"
         to="/api/item/popular"
     ></items-api>
 
-    {{-- Anchor --}}
-    <a name="title"></a>
-
-    {{-- Home Section --}}
-    @isset($home_section)
-        <section class="position-relative">
-            @admin
-                <a href="#!" class="btn btn-success position-absolute rounded-circle edit-form-btn"
-                    style="top:20px; right:20px;"
-                    data-form="home-form"
-                    data-section="home-section"
-                >
-                    <i class="fas fa-pen fa-1x"></i>
-                </a>
-            @endadmin
-            @admin
-                {{-- Form --}}
-                <form action="{{ action('Admin\SectionController@update', ['section' => $home_section['id']]) }}"
-                    class="px-4 d-none mt-3 editing-form"
-                    method="post"
-                    id="home-form"
-                >
-                    @csrf @method('put')
-                    <div class="form-group">
-                        <input type="text" 
-                            name="title"
-                            value="{{ $home_section['title'] }}"
-                            class="form-control text-center"
-                        >
-                    </div>
-                    <div class="form-group">
-                        <textarea name="content"
-                            class="form-control"
-                        >{{ $home_section['content'] }}</textarea>
-                    </div>
-                    <div class="form-group text-center">
-                        <button type="submit"
-                            class="btn btn-success position-absolute rounded-circle edit-form-btn" 
-                            title="@lang('contacts.save')"
-                            style="top:20px; right:75px;"
-                        >
-                            <i class="fas fa-save fa-1x"></i>
-                        </button>
-                    </div>
-                </form>
-            @endadmin
-            <div id="home-section">
-                {{-- Title --}}
-                @if (!empty($home_section['title']))
-                    <h3 class="display-4 text-center p-4">
-                        {{ $home_section['title'] }}
-                    </h3>
-                @endif
-                {{-- Content --}}
-                @if (!empty($home_section['content']))
-                    <p class="text-justify px-5 pb-5" style="font-size:1.2em">
-                        {!! nl2br($home_section['content']) !!}
-                    </p>
-                @endif
-            </div>
-        </section>
+    {{-- Home Down Section --}}
+    @isset($home_sections)
+        @include('includes.section', [
+            'section' => $home_sections[1],
+            'anchor' => 'home_down',
+        ])
     @endisset
 </div>
 
