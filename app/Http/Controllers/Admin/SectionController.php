89866416eb3b90285ac645b5e8efedcf8bae4a14
SerhiiCho <serhiicho@inbox.ru>
2018-12-28 16:51:03 +0200

    -1- Added SectionRequet to a SectionController update method

--- app/Http/Controllers/Admin/SectionController.php
+++ app/Http/Controllers/Admin/SectionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SectionRequest;
 use App\Models\Section;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -12,13 +13,17 @@ class SectionController extends Controller
     /**
      * Update section in database
      *
-     * @param \Illuminate\Http\Request $request
+     * @param \App\Http\Requests\SectionRequest $request
      * @param \App\Models\Section $section
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, Section $section): RedirectResponse
+    public function update(SectionRequest $request, Section $section): RedirectResponse
     {
-        $section->update($request->only(['title', 'content']));
+        $section->update([
+            'title' => request('title'),
+            'content' => request('content'),
+            'name' => $section->name,
+        ]);
         return back();
     }
 }
