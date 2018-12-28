dd3c32d9f8cbbb080470df4571fddc6de68b614e
SerhiiCho <serhiicho@inbox.ru>
2018-12-28 15:29:46 +0200

    -2- Added google like switches to admin dashboard

--- resources/views/admin/dashboard/partials/settings.blade.php
+++ resources/views/admin/dashboard/partials/settings.blade.php
@@ -5,51 +5,54 @@
 
 <table class="table">
     <tr> {{-- Registration option --}}
-        <td>@lang('options.allow_registration')</td>
         <td>
             <form action="{{ action('Admin\OptionController@registration') }}" method="post">
                 @csrf @method('put')
                 <input type="checkbox"
-                    class="auto-submit-cb"
+                    class="auto-submit-cb switch"
+                    id="switch1"
                     name="option"
                     {{ $admin_options['registration'] ? 'checked' : '' }}
                 >
+                <label for="switch1">@lang('options.allow_registration')</label>
             </form>
         </td>
     </tr>
     <tr> {{-- Men category option --}}
-        <td>@lang('options.turn_on_men_category')</td>
         <td>
             <form action="{{ action('Admin\OptionController@menCategory') }}" method="post">
                 @csrf @method('put')
                 <input type="checkbox"
                     name="option" 
-                    class="auto-submit-cb"
+                    id="switch2"
+                    class="auto-submit-cb switch"
                     {{ $admin_options['men_category'] ? 'checked' : '' }}
                 >
+                <label for="switch2">@lang('options.turn_on_men_category')</label>
             </form>
         </td>
     </tr>
     <tr> {{-- Women category option --}}
-        <td>@lang('options.turn_on_women_category')</td>
         <td>
             <form action="{{ action('Admin\OptionController@womenCategory') }}" method="post">
                 @csrf @method('put')
                 <input type="checkbox"
                     name="option" 
-                    class="auto-submit-cb"
+                    id="switch3"
+                    class="auto-submit-cb switch"
                     {{ $admin_options['women_category'] ? 'checked' : '' }}
                 >
+                <label for="switch3">@lang('options.turn_on_women_category')</label>
             </form>
         </td>
     </tr>
     <tr> {{-- Forget cache --}}
-        <td>@lang('options.forget_cache')</td>
-        <td>
+        <td align="left">
             <form action="{{ action('Admin\OptionController@cacheForget') }}" method="post">
                 @csrf @method('put')
-                <button type="submit" style="background:none; border:none; cursor:pointer">
-                    <i class="fas fa-trash-alt fa-1x" style="color:brown"></i>
+                <button type="submit" class="btn btn-sm clean-cache-btn">
+                    <i class="fas fa-trash-alt fa-1x"></i> 
+                    @lang('options.forget_cache')
                 </button>
             </form>
         </td>
