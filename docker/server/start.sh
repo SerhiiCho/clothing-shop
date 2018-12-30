543bd33035560295564521c4f00f5e388d725b94
SerhiiCho <serhiicho@inbox.ru>
2018-12-31 01:18:33 +0200

    -1- Changed start.sh

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-if [ ! -f /var/www/.env ]; then
+if [ ! -f /var/www/.env ] [ ! -f /var/www/vendor/autoload.php ]; then
     cd /var/www
     cp .env.example .env
     php artisan key:generate
     php artisan wipe
     ./vendor/bin/phpunit
+else
+    echo 'Try again, because composer is not done yet'
 fi
