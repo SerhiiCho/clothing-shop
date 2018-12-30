7c72ee9e40845e58d4e22af2a39304b9d0b52c7c
SerhiiCho <serhiicho@inbox.ru>
2018-12-31 01:20:16 +0200

    -1- start.sh

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ ! -f /var/www/.env ] [ ! -f /var/www/vendor/autoload.php ]; then
+if [ ! -f /var/www/.env ] && [ ! -f /var/www/vendor/autoload.php ]; then
     cd /var/www
     cp .env.example .env
     php artisan key:generate
