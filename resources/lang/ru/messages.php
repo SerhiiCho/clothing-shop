e0c116a62a4948a1dd5ea0faecd64d7cc028be53
SerhiiCho <serhiicho@inbox.ru>
2019-01-03 23:02:21 +0200

    -1- Also added needed buttons to orders/index page, buttons such as Untake order, Take order and status bar on a card

--- resources/lang/ru/messages.php
+++ resources/lang/ru/messages.php
@@ -5,12 +5,19 @@ return [
     // Controllers
     'order_deleted' => 'Заказ удален',
     'order_closed' => 'Заказ закрыт',
+    'take_order' => 'Взять заказ',
+    'order_already_has_user' => 'Этот заказ выполняет :user',
+    'you_took_the_order' => 'Вы взяли заказ # :order',
+    'cant_close_order' => 'Для того чтобы закрыть заказ, его нужно добавить в список выполняемых',
+    'you_deleted_the_order' => 'Вы убрали заказ # :order',
 
     // Views
+    'taken_by' => 'Выполняет',
     'more_clothes' => 'Еще товары',
     'welcome' => 'Добро пожаловать',
     'back' => 'Назад',
     'close_order' => 'Закрыть заказ',
+    'untake_order' => 'Убрать заказ',
     'close_this_order' => 'Закрыть этот заказ?',
     'delete_this_order' => 'Удалить этот заказ?',
     'have_no_rights' => 'Доступ к странице которую вы запрашиваете открыт только для подтвержденных, зарегистрированных пользователей с определленными правами',
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -28,25 +28,64 @@
     <div class="row pb-5">
         @forelse ($orders as $order)
             <div class="col-sm-6 col-lg-4 col-xl-3 mb-4">
-                <div class="card" style="border-top:4px solid {{ $tab == 'closed' ? '#e56114' : 'green' }}">
+                <div class="card position-relative" 
                     @if ($tab == 'closed')
+                        style="border-top:4px solid #e56114"
+                    @elseif ($order->user()->exists())
+                        style="border-top:4px solid #8788e0"
+                    @else
+                        style="border-top:4px solid green"
+                    @endif
+                >
+                    @if ($tab == 'closed')
+                        {{-- Delete order --}}
                         <form action="{{ action('Admin\OrderController@destroy', ['id' => $order->id]) }}" method="post">
                             @csrf @method('delete')
                             <button type="submit"
-                                class="py-2 confirm btn btn-sm btn-block"
+                                class="py-2 confirm btn btn-sm btn-block bg-transparent"
                                 data-confirm="@lang('messages.delete_this_order')"
-                                style="background:transparent; color:brown"
+                                style="color:brown"
                             >
                                 <i class="fas fa-trash-alt"></i> @lang('messages.delete_order')
                             </button>
                         </form>
                     @else
+                        {{-- Status order taken by --}}
+                        @if ($order->user()->exists() && !$order->isTakenBy(user()))
+                            <span class="btn btn-sm py-2" style="color:#6567da">
+                                <i class="fas fa-sync-alt fa-spin"></i> @lang('messages.taken_by'): 
+                                {{ $order->user->name }}
+                            </span>
+                        @else
+                            <form action="{{ action('Admin\OrderController@store', ['id' => $order->id]) }}" method="post">
+                                @csrf
+                                @if ($order->isTakenBy(user()))
+                                    {{-- Untake order --}}
+                                    <button type="submit"
+                                        class="py-2 btn btn-sm btn-block bg-transparent"
+                                        style="color:#e56114"
+                                    >
+                                        <i class="fas fa-times"></i> @lang('messages.untake_order')
+                                    </button>
+                                @else
+                                    {{-- Take order --}}
+                                    <button type="submit"
+                                        class="py-2 btn btn-sm btn-block bg-transparent"
+                                        style="color:#106d17"
+                                    >
+                                        <i class="fas fa-check"></i> @lang('messages.take_order')
+                                    </button>
+                                @endif
+                            </form>
+                        @endif
+                        {{-- Close order --}}
                         <form action="{{ action('Admin\OrderController@softDelete', ['id' => $order->id]) }}" method="post">
                             @csrf @method('put')
                             <button type="submit"
-                                class="py-2 confirm btn btn-sm btn-block"
+                                class="confirm btn btn-sm btn-block bg-transparent"
                                 data-confirm="@lang('messages.close_this_order')"
-                                style="background:transparent; color:#e56114"
+                                style="color:#e56114"
+                                {{ $order->isTakenBy(user()) ? '' : 'disabled' }}
                             >
                                 <i class="fas fa-ban"></i> @lang('messages.close_order')
                             </button>
