9beec3f1253384a76fceb422545c13661787eefe
SerhiiCho <serhiicho@inbox.ru>
2019-01-04 20:54:22 +0200

    -1- Added temporary job for testing purposes and deleted composer from Dockerfile

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')
     ->middleware('admin');
 
+Route::get('test', function () {
+    \App\Jobs\BackupJob::dispatch();
+});
+
 Route::prefix(config('custom.enter_slug'))->group(function () {
     Auth::routes();
 });
