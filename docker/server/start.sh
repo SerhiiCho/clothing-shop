f5070482cec665ba63433d6bd5a1dd062cde255a
SerhiiCho <serhiicho@inbox.ru>
2019-01-04 20:16:52 +0200

    -1- Movfed  line from Dockerfile to start.sh Also changed conf file laravel-worker

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 
-if [ ! -f /var/www/.env ] && [ -f /var/www/vendor/autoload.php ]; then
+echo 'Waiting for 5 seconds'
+sleep 5
+
+if [ -f /var/www/vendor/autoload.php ]; then
     cd /var/www
-    cp .env.example .env
-    php artisan key:generate
     php artisan wipe
-    ./vendor/bin/phpunit
+    supervisord -c /etc/supervisor/conf.d/laravel-worker.conf
+
+    if [ ! -f /var/www/.env ]; then
+        cp .env.example .env
+        php artisan key:generate
+        php artisan wipe
+    else
+        echo 'Seems like .env file is already created'
+    fi
 else
-    echo 'Try again, because composer is still installing dependencies'
+    echo 'Try again, vendor/autoload.php is not created yet'
 fi
