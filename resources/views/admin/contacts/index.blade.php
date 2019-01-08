e4c2fe344ea65d220d0482d88317f65d99dc91ba
SerhiiCho <serhiicho@inbox.ru>
2019-01-08 15:54:47 +0200

    -1- Formatted HTML on cards/index page

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="container pb-5">
-    @isset($contacts)
+    @if (count($contacts) > 0)
         <h4 class="display-4 text-center p-3">
             @lang('user-sidebar.contacts'): 
             {{ count($contacts) }}
@@ -14,7 +14,7 @@
         <div class="row">
             <div class="col-12 col-lg-8 offset-lg-2">
                 <ul class="list-group mb-4">
-                    @forelse($contacts as $contact)
+                    @foreach ($contacts as $contact)
                         <li class="list-group-item d-flex justify-content-between align-items-center">
 
                             {{-- Icon --}}
@@ -55,21 +55,22 @@
                                 @endadmin
                             </div>
                         </li>
-                    @empty
-                        <p class="text-center">@lang('contacts.no_contacts')</p>
-                    @endforelse
+                    @endforeach
                 </ul>
-                <div class="text-center">
-                    <a href="/admin/contacts/create"
-                        title="@lang('contacts.add_contact')"
-                        class="btn btn-success"
-                    >
-                        @lang('contacts.add_contact')
-                    </a>
-                </div>
             </div>
         </div>
-    @endisset
+    @else
+        <p class="text-center pt-5 pb-3">@lang('contacts.no_contacts')</p>
+    @endif
+
+    <div class="text-center">
+        <a href="/admin/contacts/create"
+            title="@lang('contacts.add_contact')"
+            class="btn btn-success"
+        >
+            @lang('contacts.add_contact')
+        </a>
+    </div>
 </div>
 
 @endsection
