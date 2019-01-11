67f9298d4a8be7106e3a18c6ebb4418169bd7f04
SerhiiCho <serhiicho@inbox.ru>
2019-01-11 18:49:57 +0200

    -1- Added supervisor

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo 'Setting everythigng up'
+echo 'Waiting for 3 seconds'
 sleep 3
 
 if [ -f /var/www/vendor/autoload.php ]; then
@@ -9,10 +9,15 @@ if [ -f /var/www/vendor/autoload.php ]; then
     if [ ! -f /var/www/.env ]; then
         cp .env.example .env
         php artisan key:generate
+        php artisan storage:link
         php artisan wipe
     else
         echo 'Seems like .env file is already created'
     fi
+
+    if [ -f /etc/supervisor/conf.d/laravel-worker.conf ]; then
+        supervisord && supervisorctl update && supervisorctl start laravel-worker:*
+    fi
 else
-    echo 'Try again, vendor/autoload.php is not created yet'
+    echo 'Wait couple seconds and try again. vendor/autoload.php file is not created yet, composer is currently installing it.'
 fi
