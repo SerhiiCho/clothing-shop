a4870c7f4b8df5099c8449250f8cfc5826fd85a1
SerhiiCho <serhiicho@inbox.ru>
2019-01-11 18:04:05 +0200

    -0-  Renamed all container names in docker-compose file And changed start.sh file

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -1,17 +1,14 @@
 #! /bin/bash
 
-echo 'Waiting for 5 seconds'
-sleep 5
+echo 'Setting everythigng up'
+sleep 3
 
 if [ -f /var/www/vendor/autoload.php ]; then
     cd /var/www
-    php artisan wipe
 
     if [ ! -f /var/www/.env ]; then
         cp .env.example .env
         php artisan key:generate
-
-        sleep 2
         php artisan wipe
     else
         echo 'Seems like .env file is already created'
