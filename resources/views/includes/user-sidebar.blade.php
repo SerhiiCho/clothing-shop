cffc47ce369d7711f8eae79e7ce3883dfc90f422
SerhiiCho <serhiicho@inbox.ru>
2019-01-16 15:30:35 +0200

    -1- In user-sidebar.blade deleted all_items link and added table link

--- resources/views/includes/user-sidebar.blade.php
+++ resources/views/includes/user-sidebar.blade.php
@@ -7,6 +7,7 @@
                     <span>@lang('user-sidebar.add_new_item')</span>
                 </a>
             </li>
+
             <li class="{{ active_if_route_is('admin/orders') }}">
                 <a href="/admin/orders" title="@lang('user-sidebar.orders')">
                     @if ($orders > 0)
@@ -16,18 +17,21 @@
                     <span>@lang('user-sidebar.orders')</span>
                 </a>
             </li>
+
             <li class="{{ active_if_route_is('admin/cards') }}">
                 <a href="/admin/cards" title="@lang('user-sidebar.cards')">
                     <i class="fas fa-portrait icon-profile-menu-line"></i>
                     <span>@lang('user-sidebar.cards')</span>
                 </a>
             </li>
+
             <li class="{{ active_if_route_is('slider') }}">
                 <a href="/admin/slider" title="@lang('user-sidebar.slider')">
                     <i class="fas fa-images icon-profile-menu-line"></i>
                     <span>@lang('user-sidebar.slider')</span>
                 </a>
             </li>
+
             <li class="{{ active_if_route_is('contacts') }}">
                 <a href="/admin/contacts" title="@lang('user-sidebar.contacts')">
                     <i class="fas fa-phone icon-profile-menu-line"></i>
@@ -49,25 +53,28 @@
                 </li>
             @endmaster
 
-            <li class="{{ active_if_route_is('items') }}" >
-                <a href="/items" title="@lang('user-sidebar.all_items')">
-                    <i class="fas fa-tshirt icon-profile-menu-line"></i>
-                    <span>@lang('user-sidebar.all_items')</span>
-                </a>
-            </li>
             <li class="{{ active_if_route_is('logs') }}" >
                 <a href="/logs" title="@lang('logs.logs')">
                     <i class="fas fa-file-contract icon-profile-menu-line"></i>
                     <span>@lang('logs.logs')</span>
                 </a>
             </li>
+
             <li class="{{ active_if_route_is('admin/dashboard') }}">
                 <a href="/admin/dashboard" title="@lang('dashboard.dashboard')">
                     <i class="fas fa-wrench icon-profile-menu-line"></i>
                     <span>@lang('dashboard.dash')</span>
                 </a>
             </li>
+
+            <li class="{{ active_if_route_is('admin/table') }}">
+                <a href="/admin/table" title="@lang('table.table')">
+                    <i class="fas fa-table icon-profile-menu-line"></i>
+                    <span>@lang('table.table')</span>
+                </a>
+            </li>
         </ul>
+
         <li>
             <a href="{{ route('logout') }}" title="@lang('user-sidebar.exit')" id="logout-btn">
                 <i class="fas fa-sign-out-alt icon-profile-menu-line"></i>
