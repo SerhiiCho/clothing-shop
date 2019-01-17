4dbfea8a100a98ade08c7b90d93153f439df90d3
SerhiiCho <serhiicho@inbox.ru>
2019-01-17 20:08:19 +0200

    -1- In statistics.blade.php added coalesce operator to variables

--- resources/views/admin/dashboard/partials/statistics.blade.php
+++ resources/views/admin/dashboard/partials/statistics.blade.php
@@ -6,10 +6,10 @@
 <table class="table table-hover">
     <tr>
         <td>@lang('items.all_men_items'):</td>
-        <td>{{ $all_men_items }}</td>
+        <td>{{ $all_men_items ?? '' }}</td>
     </tr>
     <tr>
         <td>@lang('items.all_women_items'):</td>
-        <td>{{ $all_women_items }}</td>
+        <td>{{ $all_women_items ?? '' }}</td>
     </tr>
 </table>
