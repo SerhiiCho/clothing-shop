095e7b91ef4c99ba3c28a6121217a6d847292664
SerhiiCho <serhiicho@inbox.ru>
2019-01-17 20:04:06 +0200

    -2- Refactored DashboardController@index method Added try/catch block and caching the data

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Item;
+use Illuminate\Database\QueryException;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
@@ -21,9 +22,18 @@ class DashboardController extends Controller
      */
     public function index(): View
     {
-        return view('admin.dashboard.index', [
-            'all_men_items' => Item::whereCategory('men')->sum('stock'),
-            'all_women_items' => Item::whereCategory('women')->sum('stock'),
-        ]);
+        try {
+            return view('admin.dashboard.index', [
+                'all_men_items' => cache()->rememberForever('all_men_items', function () {
+                    return Item::whereCategory('men')->sum('stock');
+                }),
+                'all_women_items' => cache()->rememberForever('all_women_items', function () {
+                    return Item::whereCategory('women')->sum('stock');
+                }),
+            ]);
+        } catch (QueryException $e) {
+            no_connection_error($e, __CLASS__);
+            return view('admin.dashboard.index');
+        }
     }
 }
