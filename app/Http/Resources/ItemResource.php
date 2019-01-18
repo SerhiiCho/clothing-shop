16830214585d33c2fa53c3150dda498652155a12
SerhiiCho <serhiicho@inbox.ru>
2019-01-18 23:08:28 +0200

    -0- Added comments to Resource files

--- app/Http/Resources/ItemResource.php
+++ app/Http/Resources/ItemResource.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ItemResource extends JsonResource
 {
     /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function toArray($request): array
