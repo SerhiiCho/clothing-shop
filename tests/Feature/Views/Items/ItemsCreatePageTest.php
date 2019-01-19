b6f5c06c38ec574fc0d6c1a60e81027d6bc5da4a
SerhiiCho <serhiicho@inbox.ru>
2019-01-19 18:14:20 +0200

    -2- Changed ItemsCreatePageTest and added tests for testing uploading and deleting image file

--- tests/Feature/Views/Items/ItemsCreatePageTest.php
+++ tests/Feature/Views/Items/ItemsCreatePageTest.php
@@ -2,13 +2,26 @@
 
 namespace Tests\Feature\Views\Items;
 
+use App\Models\Item;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class ItemsCreatePageTest extends TestCase
 {
     use DatabaseTransactions;
+
+    private $admin;
+
+    /**
+     * @author Cho
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->admin = factory(User::class)->state('admin')->create();
+    }
 
     /**
      * @author Cho
@@ -37,9 +50,7 @@ class ItemsCreatePageTest extends TestCase
      */
     public function page_is_accessable_by_admin(): void
     {
-        $admin = factory(User::class)->state('admin')->create();
-
-        $this->actingAs($admin)
+        $this->actingAs($this->admin)
             ->get("/items/create")
             ->assertOk()
             ->assertViewIs('items.create');
@@ -51,7 +62,88 @@ class ItemsCreatePageTest extends TestCase
      */
     public function admin_can_add_new_items(): void
     {
-        $admin = factory(User::class)->state('admin')->create();
+        $form_data = $this->generateFormData(false);
+
+        $this->actingAs($this->admin)
+            ->post(action('ItemController@store'), $form_data)
+            ->assertRedirect();
+
+        $this->assertDatabaseHas('items', [
+            'title' => $form_data['title'],
+        ]);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function admin_can_upload_image(): void
+    {
+        $form_data = $this->generateFormData(true);
+
+        $this->actingAs($this->admin)
+            ->post(action('ItemController@store'), $form_data)
+            ->assertRedirect();
+
+        $item = Item::whereTitle($form_data['title'])->first();
+        $photo = $item->photos()->first()->name;
+
+        $this->assertEquals(1, $item->photos()->count());
+        $this->assertNotEquals('default.jpg', $photo);
+        $this->assertImagesExist($photo);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function admin_can_change_image(): void
+    {
+        $form_data = $this->generateFormData(true);
+        $item = factory(Item::class)->create();
+
+        $this->actingAs($this->admin)
+            ->put(action('ItemController@update', [
+                'id' => $item->slug,
+            ]), $form_data)
+            ->assertRedirect();
+
+        $this->assertEquals(1, $item->photos()->count());
+        $this->assertImagesExist($item->photos()->first()->name);
+    }
+
+    /**
+     * @author Cho
+     * @test
+     */
+    public function image_is_deleted_with_the_item(): void
+    {
+        $form_data = $this->generateFormData(true);
+
+        // Create item with image
+        $this->actingAs($this->admin)
+            ->post(action('ItemController@store'), $form_data)
+            ->assertRedirect();
+
+        $item = Item::whereTitle($form_data['title'])->first();
+        $photo = $item->photos()->first()->name;
+
+        // Delete items with photos
+        $this->actingAs($this->admin)->delete("/api/item/{$item->slug}");
+
+        $this->assertEquals(0, $item->photos()->count());
+        $this->assertFileNotExists(storage_path("app/public/img/big/clothes/{$photo}"));
+        $this->assertFileNotExists(storage_path("app/public/img/small/clothes/{$photo}"));
+    }
+
+    /**
+     * Method helper
+     *
+     * @param bool $with_photo
+     * @return array
+     */
+    private function generateFormData(bool $with_photo): array
+    {
         $form_data = [
             'title' => str_random(7),
             'content' => str_random(12),
@@ -61,11 +153,25 @@ class ItemsCreatePageTest extends TestCase
             'price' => rand(1, 10000),
         ];
 
-        $this->actingAs($admin)
-            ->post(action('ItemController@store'), $form_data);
+        if ($with_photo) {
+            $form_data['photos'] = [UploadedFile::fake()->image('image.jpg')];
+        }
 
-        $this->assertDatabaseHas('items', [
-            'title' => $form_data['title'],
+        return $form_data;
+    }
+
+    /**
+     * @param string $photo
+     * @return void
+     */
+    private function assertImagesExist(string $photo): void
+    {
+        $this->assertFileExists(storage_path("app/public/img/big/clothes/{$photo}"));
+        $this->assertFileExists(storage_path("app/public/img/small/clothes/{$photo}"));
+
+        \Storage::delete([
+            "public/img/big/clothes/{$photo}",
+            "public/img/small/clothes/{$photo}",
         ]);
     }
 }
