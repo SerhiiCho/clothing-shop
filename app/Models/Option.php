87bf1f03b736a232ab8b936ec45338322f6afd1a
SerhiiCho <serhiicho@protonmail.com>
2019-08-21 19:04:35 +0300

    Removed doc block from Option modal

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -16,12 +16,6 @@ class Option extends Model
      */
     public $timestamps = false;
 
-    /**
-     * @param string $option
-     * @param int $value
-     * @return void
-     * @throws \Exception
-     */
     public static function set(string $option, int $value): void
     {
         cache()->forget('admin_options');
